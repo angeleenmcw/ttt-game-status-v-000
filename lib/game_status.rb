fb1817dbb97bbdb359800d3590a021aa49afdd77
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2019-12-24 08:25:43 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,5 +8,8 @@ def WIN_COMBINATIONS(board)
   board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
   top_row_win = [[0,1,2],
   [3,4,5],[6,7,9]]
-  
-  
+  if board[top_row_win[0]] == "X" &&
+    board[top_row_win[1]] == "X" &&
+    board[top_row_win[2]] == "X"
+    "X won in the top row"
+  end
